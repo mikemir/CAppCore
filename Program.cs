dd0a8895430d628d14bb99c3c45603995ade6b61
Michael Emir <devmikemir@gmail.com>
2018-02-23 23:20:50 -0600

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CAppCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: validar entrada de datos
+            Console.WriteLine("Digita tu nombre: ");
+            var nombre = Console.ReadLine();
+
+            Console.WriteLine($"Hello {nombre}");
+        }
+    }
+}
